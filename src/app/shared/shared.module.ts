76b2eb6bfd8eb15ef036ce3b768b9d1810ef4b8b
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-02-19 02:13:19 -0600

    ✅ create shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+import { ErrorComponent } from './error/error.component';
+
+@NgModule({
+  declarations: [MenuComponent, ErrorComponent],
+  imports: [CommonModule],
+  exports: [MenuComponent],
+})
+export class SharedModule {}
