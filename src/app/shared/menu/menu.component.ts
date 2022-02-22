18382b33852e622c07e60ff2a429f482358f1821
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-02-21 23:38:39 -0600

    ✅shared module components

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -7,7 +7,5 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
   status: boolean = false;
-  mostrar() {
-    this.status = !this.status;
-  }
+  mostrar() { this.status = !this.status; }
 }
