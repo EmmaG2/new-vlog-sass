ce891697b22c5e299a69ce1ddbce639daac9466f
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-02-21 23:37:29 -0600

    ✅pages module components

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ContactoComponent } from '@pages/contacto/contacto.component';
+import { AboutComponent } from '@pages/about/about.component';
+import { PagesRoutingModule } from '@pages/pages-routing.module';
+import { HomeComponent } from '@pages/home/home.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ContactoComponent, AboutComponent, HomeComponent],
+  imports: [CommonModule, PagesRoutingModule],
 })
-export class PagesModule { }
+export class PagesModule {}
