4040a778c480d0d5331cdd1ab7f53cccc4eb2d4c
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-02-21 23:36:11 -0600

    ✅ create shared interfaces

---
+++ src/app/shared/interfaces/menu.interface.ts
@@ -0,0 +1,9 @@
+export interface menu {
+  textLink  : string;
+  classIcon : string;
+  classItem : string;
+  classHover: string;
+  classLink : string;
+  routerLink: string;
+  activeLink: string;
+}
