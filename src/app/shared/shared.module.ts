a71bef767720a2ecf4ac65ffff0ecd958fbb2cbf
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-02-21 21:44:49 -0600

    ✅ create footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
+import { BackgroundComponent } from './background/background.component';
 import { ErrorComponent } from './error/error.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [MenuComponent, ErrorComponent],
+  declarations: [
+    MenuComponent,
+    BackgroundComponent,
+    ErrorComponent,
+    FooterComponent,
+  ],
   imports: [CommonModule],
-  exports: [MenuComponent],
+  exports: [MenuComponent, BackgroundComponent, FooterComponent],
 })
 export class SharedModule {}
