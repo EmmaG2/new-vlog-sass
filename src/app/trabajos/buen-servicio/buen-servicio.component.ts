441fa21c90ac85eee03a8f0736764c9295ffec3f
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-03-01 18:14:05 -0600

    trabajos module

---
+++ src/app/trabajos/buen-servicio/buen-servicio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buen-servicio',
+  templateUrl: './buen-servicio.component.html',
+  styleUrls: ['./buen-servicio.component.scss']
+})
+export class BuenServicioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/trabajos/cuadro-sinoptico/cuadro-sinoptico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cuadro-sinoptico',
+  templateUrl: './cuadro-sinoptico.component.html',
+  styleUrls: ['./cuadro-sinoptico.component.scss']
+})
+export class CuadroSinopticoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
