810b1150499da8659000819466d0b7a1a57402a9
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-03-02 13:07:18 -0600

    tipos clientes component

---
+++ src/app/core/views/trabajos/tipos-clientes/tipos-clientes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tipos-clientes',
+  templateUrl: './tipos-clientes.component.html',
+  styleUrls: ['./tipos-clientes.component.scss']
+})
+export class TiposClientesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
