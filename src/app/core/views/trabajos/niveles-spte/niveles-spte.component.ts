e4e6e0c0a34fd4fd5b8445f19c8396c2bfe43f52
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-03-02 13:06:56 -0600

    niveles component

---
+++ src/app/core/views/trabajos/niveles-spte/niveles-spte.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-mental',
+  templateUrl: './niveles-spte.component.html',
+  styleUrls: ['./niveles-spte.component.scss']
+})
+export class NivelesSpteComponent {}
