0e0fcb55aa3e4a66f54dfbfae80599f66ced4240
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-03-02 13:06:10 -0600

    cliente component

---
+++ src/app/core/views/trabajos/atencion-cliente/atencion-cliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atencion-cliente',
+  templateUrl: './atencion-cliente.component.html',
+  styleUrls: ['./atencion-cliente.component.scss']
+})
+export class AtencionClienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
