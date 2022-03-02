d621ab1b5478e4ab8a8a29a93858e6a1567f8625
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-03-02 13:06:33 -0600

    conceptos component

---
+++ src/app/core/views/trabajos/conceptos-spte-tecnico/conceptos-spte-tecnico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conceptos-spte-tecnico',
+  templateUrl: './conceptos-spte-tecnico.component.html',
+  styleUrls: ['./conceptos-spte-tecnico.component.scss']
+})
+export class ConceptosSpteTecnicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
