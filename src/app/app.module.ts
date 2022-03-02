cef8ee71b5b10f88118479203b5679a2981bf914
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-03-02 13:09:02 -0600

    app component updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { SharedModule } from './shared/shared.module';
-import { PagesModule } from './pages/pages.module';
+import { SharedModule } from '@shared/shared.module';
+import { PagesModule } from '@core/pages.module';
 
 @NgModule({
   declarations: [AppComponent],
