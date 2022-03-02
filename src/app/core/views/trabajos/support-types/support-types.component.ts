22a91115dec327c0f7862d937a08a079f444190d
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-03-02 13:07:04 -0600

    tipos component

---
+++ src/app/core/views/trabajos/support-types/support-types.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support-types',
+  templateUrl: './support-types.component.html',
+  styleUrls: ['./support-types.component.scss']
+})
+export class SupportTypesComponent {
+
+
+}
