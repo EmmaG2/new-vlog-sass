8270af5ad6108925dc84c9198412146c167dfd4f
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-03-02 13:06:22 -0600

    atributos component

---
+++ src/app/core/views/trabajos/atributos-atencion-cliente/atributos-atencion-cliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atributos-atencion-cliente',
+  templateUrl: './atributos-atencion-cliente.component.html',
+  styleUrls: ['./atributos-atencion-cliente.component.scss']
+})
+export class AtributosAtencionClienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
