e53953ca070b38444fb5b1aad771c91d3a6acacf
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-03-09 23:17:13 -0600

    ✅ created tests folder

---
+++ tests/home.component.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HomeComponent } from '../src/app/core/views/home/home.component';
+
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        HomeComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
