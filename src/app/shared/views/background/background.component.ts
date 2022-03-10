8ca58fdb6e896ea6aa656c92d5c008b1f9e1260d
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-03-09 23:18:29 -0600

    ✅typed.js added

--- src/app/shared/views/background/background.component.ts
+++ src/app/shared/views/background/background.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.scss']
 })
-export class BackgroundComponent {
+export class BackgroundComponent implements OnInit {
 
+  ngOnInit(): void {
+    const settings = new Typed('.typed', {
+      strings: [
+        'Ezra',
+        'Cristianos',
+        'Profesionales'
+      ],
+      smartBackspace: true,
+      loop: true,
+      backSpeed: 200,
+      showCursor: false,
+      startDelay: 100
+    })
+  }
 }
